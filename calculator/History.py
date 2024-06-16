58dadc5b6d2a5592af7bbd0c5f4a4f8ff1225439
Racheal Ogungbile <rao22@njit.edu>
2024-06-15 23:21:01 -0400

    setting up repo

---
+++ calculator/History.py
@@ -0,0 +1,12 @@
+from calculator.calculation import Calculation as calc
+
+class Calculator_History:
+    log: list[calc] = []
+
+    @classmethod
+    def addCalculation(cls, Calc):
+        return cls.log.append(Calc)
+    
+    @classmethod
+    def getLastCalculation(cls):
+        return cls.log[len(cls.log)-1]
